31c9e6d5a7765ac8b6235cfc377cc24091486b40
Peter Allen <peterjeremy@cableone.net>
2020-02-25 11:49:17 -0700

    got links back to the main page to work

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('techtips', views.tips, name = 'tips'),
+	path('about', views.about, name = 'about'),
+	path('archive', views.archive, name = 'archive'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from .models import Blog, Comment
+from time import strftime
 # Create your views here.
+
 def index(request):
 	posts = Blog.objects.order_by('-posted')
 	return render(request, 'blog/index.html',{'posts': posts [len(posts)-3:]})
+
+
+def archive(request):
+	posts = Blog.objects.order_by('-posted')
+	return render(request, 'blog/index.html',{'posts': posts})
+
+
+def tips(request):
+	return render(request, 'blog/techtips.html', {'time': strftime('%c')})
+
+
+def about(request):
+	return render(request, 'blog/about.html', {'time': strftime('%c')})
+
