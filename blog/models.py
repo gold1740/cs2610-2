5b349a634704ad29587267fff02f3a4b11bf751e
Peter Allen <peterjeremy@cableone.net>
2020-02-25 11:15:24 -0700

    started the database and got the main page mostly working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length=16)
+	author = models.CharField(max_length=16)
+	content  = models.TextField()
+	posted = models.DateTimeField()
+
+	def __str__(self):
+		return f"{self.title} by {self.author}"
+
+
+class Comment(models.Model):
+	blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	commenter = models.CharField(max_length=16)
+	email = models.EmailField()
+	content  = models.TextField()
+	posted = models.DateTimeField()
+	
+	def __str__(self):
+		return f"comment on {self.blog.title} by {self.commenter}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog, Comment
 # Create your views here.
+def index(request):
+	posts = Blog.objects.order_by('-posted')
+	return render(request, 'blog/index.html',{'posts': posts [len(posts)-3:]})
