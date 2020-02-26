6d7ff9d7422484933cd911261f03ff888cc5e891
Peter Allen <peterjeremy@cableone.net>
2020-02-25 22:53:33 -0700

    fixed most everything

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	path('techtips', views.tips, name = 'tips'),
 	path('about', views.about, name = 'about'),
 	path('archive', views.archive, name = 'archive'),
+	path('<int:post_id>/', views.post, name = 'post'),
+	path('<int:post_id>/comment/', views.comment, name = 'comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 from .models import Blog, Comment
 from time import strftime
+from django.utils import timezone
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
 	posts = Blog.objects.order_by('-posted')
-	return render(request, 'blog/index.html',{'posts': posts [len(posts)-3:]})
+	return render(request, 'blog/index.html',{'posts': posts [:3]})
 
 
 def archive(request):
@@ -20,3 +23,19 @@ def tips(request):
 def about(request):
 	return render(request, 'blog/about.html', {'time': strftime('%c')})
 
+def post(request, post_id):
+	return render(request, 'blog/entry.html', {'post': get_object_or_404(Blog, pk=post_id)})
+
+def comment(request, post_id):
+	post = get_object_or_404(Blog, pk=post_id)
+	try:
+		name = request.POST.get('name', '')
+		email = request.POST.get('email', '')
+		content = request.POST.get('content', '')
+	except(KeyError):
+		return render(request, 'blog/entry.html',{'post': post, 'error': "something went wrong",})
+	else:
+		comment = Comment(blog=post, commenter=name, email=email, content=content, posted=timezone.now())
+		comment.save()
+	return HttpResponseRedirect(f'/blog/{post_id}/')
+	
